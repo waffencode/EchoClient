f3ae0ae0e84038c4988cd65eb7aa0ae317f4fb1a
Dmitry Osobov <waffencode@gmail.com>
2025-08-28 23:52:06 +0300

    Initial commit

---
+++ EchoClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TcpClient tcpClient = new TcpClient())
+            {
+                try
+                {
+                    tcpClient.Connect("127.0.0.1", 7777);
+                }
+                catch (SocketException ex)
+                { 
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    tcpClient.Close();
+                }
+            }
+        }
+    }
+}
