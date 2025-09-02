cac004183f90184827e1d913251e5f0100576668
Dmitry Osobov <waffencode@gmail.com>
2025-09-02 17:26:13 +0300

    feat: обновлён консольный интерфейс

--- EchoClient/Program.cs
+++ EchoClient/Program.cs
@@ -1,25 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace EchoClient
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             using (TcpClient tcpClient = new TcpClient())
             {
                 try
                 {
                     tcpClient.Connect("127.0.0.1", 7777);
+                    Console.WriteLine("Подключено!");
+                    Console.WriteLine("Введите ваше сообщение. Для выхода введите 'exit'");
+                    Console.WriteLine("Для подтверждения отправки введите Enter на пустой строке.");
+
+                    while (true)
+                    {
+                        if (!tcpClient.Connected)
+                        {
+                            Console.WriteLine("Ошибка подключения... Введите 'retry' для повтора");
+                            if (Console.ReadLine().Equals("retry"))
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Клиент успешно остановлен.");
+                                return;
+                            }
+                        }
+
+
+                        StringBuilder input = new StringBuilder();
+                        string lastRead;
+                        
+                        do
+                        {
+                            lastRead = Console.ReadLine();
+                            if (lastRead.Equals("exit"))
+                            {
+                                Console.WriteLine("Клиент успешно остановлен.");
+                                return;
+                            }
+                            input.Append(lastRead);
+                        }
+                        while (lastRead != "");
+                        
+                        int bytesSent = tcpClient.Client.Send(Encoding.UTF8.GetBytes(input.ToString() + '\n'));
+                        Console.WriteLine($"Отправлено байт: {bytesSent}");
+                    }
                 }
-                catch (SocketException ex)
-                { 
-                    Console.WriteLine(ex.Message);
+                catch
+                {
+                    Console.WriteLine("Подключение разорвано... Введите 'retry' для повтора");
+                    if (Console.ReadLine().Equals("retry"))
+                    {
+                        Main();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Клиент успешно остановлен.");
+                        return;
+                    }
                 }
                 finally
                 {
